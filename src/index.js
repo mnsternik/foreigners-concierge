4ef583614f8eabe17c0260a51849e1a1364ed70a
Marcin Sternik <mnsternik@gmail.com>
2024-04-11 23:03:22 +0200

    i18e support added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,34 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import enTranslation from './i18n/en/translation.json'
+import uaTranslation from './i18n/ua/translation.json'
+import plTranslation from './i18n/pl/translation.json'
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: enTranslation,
+      },
+      pl: {
+        translation: plTranslation,
+      },
+      ua: {
+        translation: uaTranslation
+      }
+    },
+    fallbackLng: 'en', // fallback language if translation is missing
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  });
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
