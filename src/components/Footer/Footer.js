df1ccc8fed6dd6ac408ae3e8a343005eb54a0d80
Marcin Sternik <mnsternik@gmail.com>
2024-04-12 23:02:56 +0200

    Semantic corrections

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
-        <footer class="d-flex flex-wrap justify-content-center align-items-center py-3 border-top">
-            <div class="col-md-4 d-flex align-items-center justify-content-center ">
-                <span class="mb-3 mb-md-0 text-body-secondary">© 2024 Foreigners Concierge, Inc</span>
+        <footer className="d-flex flex-wrap justify-content-center align-items-center py-3 border-top">
+            <div className="col-md-4 d-flex align-items-center justify-content-center ">
+                <span className="mb-3 mb-md-0 text-body-secondary">© 2024 Foreigners Concierge, Inc</span>
             </div>
         </footer>
     )
