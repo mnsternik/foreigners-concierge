fc805317b1d5eb1029be75fe18d231d08040884a
Marcin Sternik <mnsternik@gmail.com>
2024-04-12 23:06:11 +0200

    Semantic corrections

--- src/pages/Foreigners/Foreigners.js
+++ src/pages/Foreigners/Foreigners.js
@@ -1,96 +1,68 @@
 const Foreigners = () => {
 
     return (
-        <div class="container px-4 py-5" id="featured-3">
-            <h2 class="pb-2 border-bottom">Nasza ofera dla cudzoziemców</h2>
-            <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
-                <div class="feature col">
-                    <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-                        {/* <svg class="bi" width="1em" height="1em"><use xlink:href="#collection"></use></svg> */}
+        <div className="container px-4 py-5" id="featured-3">
+            <h2 className="pb-2 border-bottom">Nasza ofera dla cudzoziemców</h2>
+            <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
+                <div className="feature col">
+                    <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                        {/* <svg className="bi" width="1em" height="1em"><use xlink:href="#collection"></use></svg> */}
                     </div>
-                    <h3 class="fs-2 text-body-emphasis">Featured title</h3>
+                    <h3 className="fs-2 text-body-emphasis">Featured title</h3>
                     <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-                    <a href="#" class="icon-link">
-                        Call to action
-                        {/* <svg class="bi"><use xlink:href="#chevron-right"></use></svg> */}
-                    </a>
                 </div>
-                <div class="feature col">
-                    <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-                        {/* <svg class="bi" width="1em" height="1em"><use xlink:href="#people-circle"></use></svg> */}
+                <div className="feature col">
+                    <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                        {/* <svg className="bi" width="1em" height="1em"><use xlink:href="#people-circle"></use></svg> */}
                     </div>
-                    <h3 class="fs-2 text-body-emphasis">Featured title</h3>
+                    <h3 className="fs-2 text-body-emphasis">Featured title</h3>
                     <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-                    <a href="#" class="icon-link">
-                        Call to action
-                        {/* <svg class="bi"><use xlink:href="#chevron-right"></use></svg> */}
-                    </a>
                 </div>
-                <div class="feature col">
-                    <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-                        {/* <svg class="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg> */}
+                <div className="feature col">
+                    <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                        {/* <svg className="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg> */}
                     </div>
-                    <h3 class="fs-2 text-body-emphasis">Featured title</h3>
+                    <h3 className="fs-2 text-body-emphasis">Featured title</h3>
                     <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-                    <a href="#" class="icon-link">
-                        Call to action
-                        {/* <svg class="bi"><use xlink:href="#chevron-right"></use></svg> */}
-                    </a>
                 </div>
-                <div class="feature col">
-                    <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-                        {/* <svg class="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg> */}
+                <div className="feature col">
+                    <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                        {/* <svg className="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg> */}
                     </div>
-                    <h3 class="fs-2 text-body-emphasis">Featured title</h3>
+                    <h3 className="fs-2 text-body-emphasis">Featured title</h3>
                     <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-                    <a href="#" class="icon-link">
-                        Call to action
-                        {/* <svg class="bi"><use xlink:href="#chevron-right"></use></svg> */}
-                    </a>
                 </div>
-                <div class="feature col">
-                    <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-                        {/* <svg class="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg> */}
+                <div className="feature col">
+                    <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                        {/* <svg className="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg> */}
                     </div>
-                    <h3 class="fs-2 text-body-emphasis">Featured title</h3>
+                    <h3 className="fs-2 text-body-emphasis">Featured title</h3>
                     <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-                    <a href="#" class="icon-link">
-                        Call to action
-                        {/* <svg class="bi"><use xlink:href="#chevron-right"></use></svg> */}
-                    </a>
                 </div>
-                <div class="feature col">
-                    <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-                        {/* <svg class="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg> */}
+                <div className="feature col">
+                    <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                        {/* <svg className="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg> */}
                     </div>
-                    <h3 class="fs-2 text-body-emphasis">Featured title</h3>
+                    <h3 className="fs-2 text-body-emphasis">Featured title</h3>
                     <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-                    <a href="#" class="icon-link">
+                    {/* <a href="#" className="icon-link">
                         Call to action
-                        {/* <svg class="bi"><use xlink:href="#chevron-right"></use></svg> */}
-                    </a>
+                        <svg className="bi"><use xlink:href="#chevron-right"></use></svg>
+                    </a> */}
                 </div>
-                <div class="feature col">
-                    <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-                        {/* <svg class="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg> */}
+                <div className="feature col">
+                    <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                        {/* <svg className="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg> */}
                     </div>
-                    <h3 class="fs-2 text-body-emphasis">Featured title</h3>
+                    <h3 className="fs-2 text-body-emphasis">Featured title</h3>
                     <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-                    <a href="#" class="icon-link">
-                        Call to action
-                        {/* <svg class="bi"><use xlink:href="#chevron-right"></use></svg> */}
-                    </a>
                 </div>
-                <div class="feature col">
-                    <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-                        {/* <svg class="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg> */}
+                <div className="feature col">
+                    <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                        {/* <svg className="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg> */}
                     </div>
-                    <h3 class="fs-2 text-body-emphasis">Featured title</h3>
+                    <h3 className="fs-2 text-body-emphasis">Featured title</h3>
                     <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-                    <a href="#" class="icon-link">
-                        Call to action
-                        {/* <svg class="bi"><use xlink:href="#chevron-right"></use></svg> */}
-                    </a>
                 </div>
             </div>
         </div>
