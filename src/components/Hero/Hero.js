02551a7f0887c81322fdb999845dc5dabc72ab7e
Marcin Sternik <mnsternik@gmail.com>
2024-04-12 23:03:59 +0200

    Handling scroll added

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,13 +4,13 @@ const Hero = (props) => {
     const { t } = useTranslation();
     
     return (
-        <div class="bg-dark text-secondary px-4 py-5 text-center">
-            <div class="py-5">
-                <h1 class="display-5 fw-bold text-white">{t('heroSection.header')}</h1>
-                <div class="col-lg-6 mx-auto">
-                    <p class="fs-5 mb-4">{t('heroSection.subHeader')}</p>
-                    <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                        <button type="button" class="btn btn-outline-light btn-lg px-4" onClick={(props.handleScroll)}>{t('heroSection.button')}</button>
+        <div className="bg-dark text-secondary px-4 py-5 text-center">
+            <div className="py-5">
+                <h1 className="display-5 fw-bold text-white">{t('heroSection.header')}</h1>
+                <div className="col-lg-6 mx-auto">
+                    <p className="fs-5 mb-4">{t('heroSection.subHeader')}</p>
+                    <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                        <button type="button" className="btn btn-outline-light btn-lg px-4" onClick={(props.handleScroll)}>{t('heroSection.button')}</button>
                     </div>
                 </div>
             </div>
