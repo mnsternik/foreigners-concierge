72cc0c4e4b227da67e81c79854b604fdabab4aa7
Marcin Sternik <mnsternik@gmail.com>
2024-04-13 23:18:02 +0200

    Handling scroll added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,22 @@
+import { useRef } from 'react';
+
 import ContactForm from '../../components/ContactForm/ContactForm';
 import About from '../About/About';
 import Hero from '../../components/Hero/Hero';
 
 const Home = () => {
+    const aboutRef = useRef(null);
+
+    const handleAboutScroll = () => {
+        aboutRef.current.scrollIntoView({ behavior: 'smooth' });
+    };
+
     return (
         <>
-            <Hero />
-            <About />
+            <Hero handleScroll={handleAboutScroll} />
+            <About targetRef={aboutRef} />
             <ContactForm />
         </>
-
     )
 }
 
